3bd10511114e219b306c6009bea9b902af552ba7
Monstrenyatko <monstrenyatko@gmail.com>
2020-03-22 13:47:26 -0700

    delete user in case of conflict with provided UID

--- run.sh
+++ run.sh
@@ -9,25 +9,43 @@ set -e
 export APP_USERNAME=transmission
 export APP_GROUPNAME=transmission
 
-if [ -n "$TRANSMISSION_DAEMON_GID" ] && [ "$TRANSMISSION_DAEMON_GID" !=  "$(id $APP_GROUPNAME -g)" ]; then
+if [ -n "$TRANSMISSION_DAEMON_UID" ]; then
+  APP_UID=$TRANSMISSION_DAEMON_UID
+fi
+
+if [ -n "$TRANSMISSION_DAEMON_GID" ]; then
+  APP_GID=$TRANSMISSION_DAEMON_GID
+fi
+
+if [ -n "$APP_GID" ] && [ "$APP_GID" != "$(id $APP_GROUPNAME -g)" ]; then
   set +e
   # delete all users using requested GID
-  cut -d: -f1,4 /etc/passwd | grep -w $TRANSMISSION_DAEMON_GID |
+  cut -d: -f1,4 /etc/passwd | grep -w $APP_GID |
   while read name_gid
   do
     name=$(echo $name_gid | cut -d: -f1)
     deluser $name
   done
   # delete group with requested GID
-  delgroup $(getent group $TRANSMISSION_DAEMON_GID | cut -d: -f1)
+  delgroup $(getent group $APP_GID | cut -d: -f1)
   set -e
   # update GID
-  groupmod --gid $TRANSMISSION_DAEMON_GID $APP_GROUPNAME
-  usermod --gid $TRANSMISSION_DAEMON_GID $APP_USERNAME
+  groupmod --gid $APP_GID $APP_GROUPNAME
+  usermod --gid $APP_GID $APP_USERNAME
 fi
 
-if [ -n "$TRANSMISSION_DAEMON_UID" ] && [ "$TRANSMISSION_DAEMON_UID" !=  "$(id $APP_USERNAME -u)" ]; then
-  usermod --uid $TRANSMISSION_DAEMON_UID $APP_USERNAME
+if [ -n "$APP_UID" ] && [ "$APP_UID" !=  "$(id $APP_USERNAME -u)" ]; then
+  set +e
+  # delete all users using requested UID
+  cut -d: -f1,3 /etc/passwd | grep -w $APP_UID |
+  while read name_uid
+  do
+    name=$(echo $name_uid | cut -d: -f1)
+    deluser $name
+  done
+  set -e
+  #
+  usermod --uid $APP_UID $APP_USERNAME
 fi
 
 if [ "$1" = 'transmission-daemon-app' ]; then
